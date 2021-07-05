324397e8677e7d60db01ba02ab940578af9ef44b
YuDongZhang <977872239@qq.com>
2021-07-05 09:36:35 +0800

    2.6,@Resource 可以单独使用,来注入变量

--- src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 /**
  * 账户的业务层实现类
  * <bean id="accountService" class="com.itheima.service.impl.AccountServiceImpl"
@@ -36,6 +38,16 @@ import org.springframework.stereotype.Component;
  *               可以是变量上，也可以是方法上
  *           细节：
  *               在使用注解注入时，set方法就不是必须的了。
+ *     Qualifier:
+ *           作用：在按照类中注入的基础之上再按照名称注入。它在给类成员注入时不能单独使用。但是在给方法参数注入时可以（稍后我们讲）
+ *           属性：
+ *               value：用于指定注入bean的id。
+ *     Resource
+ *           作用：直接按照bean的id注入。它可以独立使用
+ *           属性：
+ *               name：用于指定bean的id。
+ *       以上三个注入都只能注入其他bean类型的数据，而基本类型和String类型无法使用上述注解实现。
+ *       另外，集合类型的注入只能通过XML来实现。
  *
  * 用于改变作用范围的
  *      他们的作用就和在bean标签中使用scope属性实现的功能是一样的
@@ -46,8 +58,9 @@ import org.springframework.stereotype.Component;
 @Component("accountService")
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired//自动按照类型注入,去容器中找,然后匹配类型
-    @Qualifier("accountDao2")
+//    @Autowired//自动按照类型注入,去容器中找,然后匹配类型
+//    @Qualifier("accountDao2")
+    @Resource(name = "accountDao1")
     private IAccountDao accountDao=null;
 
     public AccountServiceImpl() {

08f4224867fb5bb64d108d6050f855840e86458d
YuDongZhang <977872239@qq.com>
2021-07-05 17:33:32 +0800

    3.4,@bean把方法返回值作为bean对象注入到spring ioc容器中

--- src/main/java/config/SpringConfiguration.java
+++ src/main/java/config/SpringConfiguration.java
@@ -1,7 +1,12 @@
 package config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 该类是一个配置类，它的作用和bean.xml是一样的
@@ -37,4 +42,31 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com.itheima")
 public class SpringConfiguration {
 
+    /**
+     * 用于创建一个QueryRunner对象
+     * @param dataSource
+     * @return
+     */
+    @Bean(name = "runner")//默认是类名,你也可以改名用 name
+    public QueryRunner createQueryRunner(DataSource dataSource){
+        return new QueryRunner(dataSource);
+    }
+
+    /**
+     * 创建数据源对象
+     * @return
+     */
+    @Bean(name = "dataSource")
+    public DataSource createDataSource(){
+        try {
+            ComboPooledDataSource ds = new ComboPooledDataSource();
+            ds.setDriverClass("com.mysql.jdbc.Driver");
+            ds.setJdbcUrl("jdbc:mysql://localhost:3306/eesy");
+            ds.setUser("root");
+            ds.setPassword("root");
+            return ds;
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
 }
